24f165a4f613225cffdf0d4a76bbb99a11d7c46b
Aejaz <Aejaz.dauwa@wns.com>
2024-05-28 20:20:22 +0530

    Extra File added

---
+++ Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [MinLength(3,ErrorMessage = "Category Name should be 5 characters atleast")]
+        [MaxLength(10)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1,1000,ErrorMessage = "Value Must be between 1 to 1000")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
